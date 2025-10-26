126faf07dd8a33b039b67751843e81fcad4681f0
kerollosmoheb <kokomoheb73@gmail.com>
2025-10-26 16:11:52 +0300

    splash view

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,11 +1,13 @@
+import 'package:dalel/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:dalel/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
-final GoRouter router = GoRouter(routes: [
-  GoRoute(
-    path: "/",
-    builder: (context, state) => const SplashView(),
+final GoRouter router = GoRouter(
+  routes: [
+    GoRoute(path: "/", builder: (context, state) => const SplashView()),
+    GoRoute(
+      path: "/onBoarding",
+      builder: (context, state) => const OnBoardingView(),
     ),
-
-],
+  ],
 );
