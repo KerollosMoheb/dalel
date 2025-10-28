61e2fb5911080424c34af6d1f945cd2971304f55
kerollosmoheb <kokomoheb73@gmail.com>
2025-10-28 04:28:00 +0300

    add firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:dalel/core/database/cache/cache_helper.dart';
 import 'package:dalel/core/routes/app_router.dart';
 import 'package:dalel/core/services/service_locator.dart';
 import 'package:dalel/core/utils/app_colors.dart';
+import 'package:dalel/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setUpServiceLocator();
   await getIt<CacheHelper>().init();
   runApp(const Dalel());
