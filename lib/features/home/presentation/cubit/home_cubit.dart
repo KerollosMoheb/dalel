b0d03d78c34460f7b3b68e1ef3b5f3704f6fb24a
kerollosmoheb <kokomoheb73@gmail.com>
2025-11-02 02:44:15 +0200

    wars subCollection

--- lib/features/home/presentation/cubit/home_cubit.dart
+++ lib/features/home/presentation/cubit/home_cubit.dart
@@ -1,13 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dalel/core/utils/app_strings.dart';
 import 'package:dalel/features/home/data/models/historical_periods_model.dart';
+import 'package:dalel/features/home/data/models/wars_model.dart';
 import 'package:dalel/features/home/presentation/cubit/home_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
   List<HistoricalPeriodsModel> historicalPeriods = [];
-  getHistoricalPeriods() async {
+  List<WarsModel> warsList = [];
+  Future<void> getHistoricalPeriods() async {
     try {
       emit(GetHistoricalPeriodsLoading());
       await FirebaseFirestore.instance
@@ -15,8 +17,9 @@ class HomeCubit extends Cubit<HomeState> {
           .get()
           .then(
             (value) => value.docs.forEach((element) async {
+              await getWarsList(element);
               historicalPeriods.add(
-                HistoricalPeriodsModel.fromJson(element.data()),
+                HistoricalPeriodsModel.fromJson(element.data(), warsList),
               );
               emit(GetHistoricalPeriodsSuccess());
             }),
@@ -24,5 +27,20 @@ class HomeCubit extends Cubit<HomeState> {
     } on Exception catch (e) {
       emit(GetHistoricalPeriodsFailure(errMessage: e.toString()));
     }
+  }
+
+  Future<void> getWarsList(
+    QueryDocumentSnapshot<Map<String, dynamic>> element,
+  ) async {
+    await FirebaseFirestore.instance
+        .collection(FireBaseStrings.historicalPeriods)
+        .doc(element.id)
+        .collection(FireBaseStrings.wars)
+        .get()
+        .then(
+          (value) => value.docs.forEach((element) {
+            warsList.add(WarsModel.fromjson(element.data()));
+          }),
+        );
   }
 }
